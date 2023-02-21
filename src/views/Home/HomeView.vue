9560155b86530d11aa6df1c6a8305013b8207bbc
M4TEO <74993888+MateoCueva03@users.noreply.github.com>
2023-02-21 13:23:35 -0500

    Update HomeView.vue

--- src/views/Home/HomeView.vue
+++ src/views/Home/HomeView.vue
@@ -29,15 +29,13 @@ export default Vue.extend({
     return {
       items: [
         {
-          src: 'https://www.epn.edu.ec/wp-content/uploads/2022/10/Banner-arriendo.jpg',
-
+          src: 'https://scontent.fuio10-1.fna.fbcdn.net/v/t1.15752-9/332517639_600440745283131_7166996402478704597_n.png?_nc_cat=102&ccb=1-7&_nc_sid=ae9488&_nc_eui2=AeGnWlmB-2sSj7Put-E27LvqY6lzJbH5ardjqXMlsflqtzc10Qbk1spo2pjvle7H_w8SqEP_0sjMuTvdRPtFKTuI&_nc_ohc=J9VjWeZDuC0AX9MvC-J&_nc_ht=scontent.fuio10-1.fna&oh=03_AdRIDPoUkam-P_jbe18QSSeox4lML591nuynrgwkRQq7gw&oe=641C686B',
         },
         {
           src: 'https://www.epn.edu.ec/wp-content/uploads/2019/09/banner-ARRIENDO-comedor-cafeteria-620x279.jpg',
         },
         {
-          src: 'https://scontent.fuio10-1.fna.fbcdn.net/v/t1.15752-9/332517639_600440745283131_7166996402478704597_n.png?_nc_cat=102&ccb=1-7&_nc_sid=ae9488&_nc_eui2=AeGnWlmB-2sSj7Put-E27LvqY6lzJbH5ardjqXMlsflqtzc10Qbk1spo2pjvle7H_w8SqEP_0sjMuTvdRPtFKTuI&_nc_ohc=J9VjWeZDuC0AX9MvC-J&_nc_ht=scontent.fuio10-1.fna&oh=03_AdRIDPoUkam-P_jbe18QSSeox4lML591nuynrgwkRQq7gw&oe=641C686B',
-
+          src: 'https://www.epn.edu.ec/wp-content/uploads/2022/10/Banner-arriendo.jpg',
         },
         {
           src: 'https://www.epn.edu.ec/wp-content/uploads/2019/01/Captura-de-pantalla-2019-01-24-a-las-9.08.02-a.m..png',
